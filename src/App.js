1df8012a8506554be921cb92b9a10c080e2b734b
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-02-28 20:02:25 -0300

    Prueba de layout+home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layaout';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>      
     </>
   );
 }
