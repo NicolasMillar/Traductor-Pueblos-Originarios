bd9de5d73e41ebfb96b6b1d3213d9ac6257d882a
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-03-01 23:39:07 -0300

    cambios para probar el githubpages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/traductor-pueblos-originarios'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     
