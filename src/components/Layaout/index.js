629a5bb4b82969b0ff95ea389aba7cc95ff3c605
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-03-07 17:33:43 -0300

    eliminacion de header

--- src/components/Layaout/index.js
+++ src/components/Layaout/index.js
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import './index.scss';
-import Header from '../Header';
 
 const Layout = () =>{
     return (
         <div className="App">
-            <Header />
             <div className='page'>
                 <Outlet/>
             </div>
